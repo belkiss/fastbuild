b7b89ba51d6244167e2bbf7bcd4e22cf2f26a487
Lambert Clara <lambert.clara@yahoo.fr>
2024-07-15 23:00:55 +0200

    add ba

--- Code/Tools/FBuild/FBuildTest/Tests/TestObject.cpp
+++ Code/Tools/FBuild/FBuildTest/Tests/TestObject.cpp
@@ -403,8 +403,8 @@ void TestObject::CacheUsingRelativePaths() const
         // Check __FILE__ paths are relative
         Tracing::OutputFormat("\n-------- PEC '%s' --------\n", buffer.Get());
         TEST_ASSERT( buffer.Find( "FILE_MACRO_START_1(./Subdir/Header.h)FILE_MACRO_END_1" ) );
+        TEST_ASSERT( !buffer.Find( "FILE_MACRO_START_1(./Subdir/Header.h)FILE_MACRO_END_1" ) );
         TEST_ASSERT( buffer.Find( "FILE_MACRO_START_2(File.cpp)FILE_MACRO_END_2" ) );
-        TEST_ASSERT ( false );
     }
 
     // Build in path B, reading from the cache
